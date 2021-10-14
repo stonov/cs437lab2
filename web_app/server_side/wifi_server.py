06774714ade57a3d2746da3206b71d3e530f35a0
Sam Hachem <hhache2@illinois.edu>
2021-10-14 17:14:18 -0500

    added the processing code for the server side

--- web_app/server_side/wifi_server.py
+++ web_app/server_side/wifi_server.py
@@ -1,7 +1,21 @@
 import socket
+import picar_4wd as fc
 
 HOST = "192.168.1.128" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
+
+def process_data(data=""):
+    if data != "":
+        if data == "forward":
+            print("moving forward")
+        elif data == "backward":
+            print("moving backward")
+        elif data == "right":
+            print("moving right")
+        elif data == "left":
+            print("moving left")
+        else:
+            print(data)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -12,9 +26,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             client, clientInfo = s.accept()
             print("server recv from: ", clientInfo)
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-            if data != b"":
-                print(data)     
-                client.sendall(data) # Echo back to client
+            process_data()
     except: 
         print("Closing socket")
         client.close()
