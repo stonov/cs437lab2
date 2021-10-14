1677d8d3df5a65f963985b5cb15b6cf425446dcd
Sam Hachem <hhache2@illinois.edu>
2021-10-14 17:23:11 -0500

    fixed the data type

--- web_app/server_side/wifi_server.py
+++ web_app/server_side/wifi_server.py
@@ -6,6 +6,7 @@ PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
 def process_data(data=""):
     if data != "":
+        data = str(data)
         if data == "forward":
             print("moving forward")
             fc.forward(power=50)
@@ -30,7 +31,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             client, clientInfo = s.accept()
             print("server recv from: ", clientInfo)
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-            process_data()
+            process_data(data)
     except: 
         print("Closing socket")
         client.close()
